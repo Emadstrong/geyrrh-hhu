8ad908403995922a454a08a3244bfa68a7dda1dc
Emadstrong <132583034+Emadstrong@users.noreply.github.com>
2024-02-25 01:42:57 +0300

    Update client.py

--- client.py
+++ client.py
@@ -26,7 +26,7 @@ class button_view(discord.ui.View):
   def __init__(self):
     super().__init__(timeout = None)
  
-  @discord.ui.button(label = "hello", custom_id= "Embed", style = discord.ButtonStyle.success)
+  @discord.ui.button(label="hello", custom_id="Embed", style=discord.ButtonStyle.success)
   async def Embed(self, interaction: discord.Interaction, button: discord.ui.Button):
     role = 1203956999239368738 
     user = interaction.user
@@ -34,8 +34,8 @@ class button_view(discord.ui.View):
       bot.role = interaction.guild.get_role(1205160396701442098)
     if bot.role not in interaction.user.roles:
       await interaction.user.add_roles(bot.role)
-      await interaction.response.send_message(f"EMAD VITK", ephemeral = True)
-    else: await interaction.response.send_message(f"Emad vitk already pressed the button", ephemeral = True)
+      await interaction.response.send_message(f"EMAD VITK", ephemeral=True)
+    else: await interaction.response.send_message(f"Emad vitk already pressed the button", ephemeral=True)
 
 bot = commands.Bot(command_prefix='.', intents=intents, help_command=None)
 
