8bbdb159a37fab194b1281ba3d4fcf993befcb2a
Emadstrong <132583034+Emadstrong@users.noreply.github.com>
2024-02-25 02:41:11 +0300

    Update client.py

--- client.py
+++ client.py
@@ -21,7 +21,8 @@ async def on_ready():
 @bot.command() 
 async def nice(ctx, arg): 
   embed=discord.Embed(title="Hello ViTK", description="nice man")
-  await ctx.respond(embed = embed, view=MyView())
+  await ctx.send(embed = embed)
+  ctx.respond(view=MyView())
   
 class button_view(discord.ui.View):
   def __init__(self):
