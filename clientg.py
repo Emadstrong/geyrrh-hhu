31cd79a239d418a067a9ec66c35857c1687ef843
Emadstrong <132583034+Emadstrong@users.noreply.github.com>
2024-02-25 13:16:28 +0300

    Create clientg.py

---
+++ clientg.py
@@ -0,0 +1,43 @@
+import discord
+import discord.ext
+import discord.ui
+from discord.ext import commands
+import os
+from dotenv import load_dotenv 
+
+load_dotenv()
+bot_token = os.environ['bot_token'] 
+
+button =[
+    interactions.ActionRow(
+        components=[
+            interactions.Button(
+                style=interactions.ButtonStyle.PRIMARY,
+                label="Click me!",
+                custom_id="click_me",
+            ),
+            interactions.Button(
+                style=interactions.ButtonStyle.DANGER,
+                label="Do not click!",
+                custom_id="do_not_click",
+            ),
+        ]
+    )
+]
+
+@client.command(
+    name="button",
+    description="Send a button",
+)
+async def _button(ctx: interactions.CommandContext):
+    await ctx.send(components=button)
+
+@client.component("click_me")
+async def click_me(ctx: interactions.ComponentContext):
+    await ctx.send("Clicked!")
+
+@client.component("do_not_click")
+async def do_not_click(ctx: interactions.ComponentContext):
+    await ctx.send("You clicked the wrong button!")
+
+client.run(bot_token)
