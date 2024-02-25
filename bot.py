e96dce68c40cdba2454dd67fb1c722a1e415200c
Emadstrong <132583034+Emadstrong@users.noreply.github.com>
2024-02-25 15:22:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ load_dotenv()
 bot_token = os.environ['bot_token']
 
 
-client = commands.Bot(command_prefix".", intents=discord.Intents.all)
+client = commands.Bot(command_prefix='.', intents=intents, help_command=None) 
 
 
 async def on_ready():
