97e7865bb729fe1c134696a8ad0ba7711ed3c110
Emadstrong <132583034+Emadstrong@users.noreply.github.com>
2024-03-01 12:21:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ load_dotenv()
 bot_token = os.environ['bot_token']
 
 
-client = commands.Bot(command_prefix="!", intents=discord.Intents.all(), help_command=None) 
+client = commands.Bot(command_prefix="!", intents=discord.Intents.default(), help_command=None) 
 
 
 async def on_ready():
@@ -35,7 +35,7 @@ async def self_roles(interaction: discord.Interaction):
 
 async def main():
   async with client:
-    await float()
+    await load()
     await client.start(bot_token)
 
 asyncio.run(main())
